e58efa2d9c79dc9c401f43a18a0516d664ee4800
Hallvard Alte Westman <hallvard.westman@gmail.com>
2023-02-28 13:16:54 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/first", () => {
+    return "";
+})
+.WithName("first")
+.WithOpenApi();
+
+app.Run();
