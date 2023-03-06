e5e798a0e7729d8360fa51fa2b1ebd87d4c2dffc
Hallvard Alte Westman <hallvard.westman@gmail.com>
2023-03-06 22:39:23 +0000

    added state class

--- src/Services/HAService.cs
+++ src/Services/HAService.cs
@@ -16,17 +16,17 @@ public class HAService : IHAService {
         if (device == null) {
             return;
         }
-        await PostEntity("Temperature", device.EntityName, device.Temperature?.Celsius,"temperature", "C");
+        await PostEntity("Temperature", device.EntityName, device.Temperature?.Celsius,"temperature", "C","measurement");
         await PostEntity("SignalStrength", device.EntityName, device.Cellular?.SignalStrength,"enum");
-        await PostEntity("DBM", device.EntityName, device.Cellular?.Dbm,"signal_strength","dBm");
-        await PostEntity("Humidity", device.EntityName, device.Humidity?.Percentage, "humidity","%");
-        await PostEntity("Light", device.EntityName, device.Light?.Lux,"illuminance", "lx");
-        await PostEntity("Battery", device.EntityName,  device.Battery?.Percentage,"battery","%");
+        await PostEntity("DBM", device.EntityName, device.Cellular?.Dbm,"signal_strength","dBm","measurement");
+        await PostEntity("Humidity", device.EntityName, device.Humidity?.Percentage, "humidity","%","measurement");
+        await PostEntity("Light", device.EntityName, device.Light?.Lux,"illuminance", "lx","measurement");
+        await PostEntity("Battery", device.EntityName,  device.Battery?.Percentage,"battery","%","measurement");
         await PostEntity("Location", device.EntityName, $"{device.Location?.Latitude},{device.Location?.Longitude}","enum");
         await PostEntity("Address", device.EntityName, device.Location?.FormattedAddress,"enum");
     }
 
-    private async Task PostEntity(string name, string entityId, object value, string deviceClass, string? unitOfMeasurement = null) {
+    private async Task PostEntity(string name, string entityId, object value, string deviceClass, string? unitOfMeasurement = null, string? stateClass = null) {
 
         using StringContent jsonContent = new(
                 JsonSerializer.Serialize(new
@@ -34,6 +34,7 @@ public class HAService : IHAService {
                     state = value,
                     attributes = new {
                         device_class = deviceClass,
+                        state_class = stateClass,
                         unit_of_measurement = unitOfMeasurement
                     }
                 },new JsonSerializerOptions()
