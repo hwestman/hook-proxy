3fda055f626447c1bb91d4d69ba01e0e1d4c5e36
Hallvard Alte Westman <hallvard.westman@gmail.com>
2023-09-17 17:19:36 +0000

    added deviceType

--- src/Services/HAService.cs
+++ src/Services/HAService.cs
@@ -16,17 +16,17 @@ public class HAService : IHAService {
         if (device == null) {
             return;
         }
-        await PostEntity("Temperature", device.EntityName, device.Temperature?.Celsius,"temperature", "C","measurement");
-        await PostEntity("SignalStrength", device.EntityName, device.Cellular?.SignalStrength,"enum");
-        await PostEntity("DBM", device.EntityName, device.Cellular?.Dbm,"signal_strength","dBm","measurement");
-        await PostEntity("Humidity", device.EntityName, device.Humidity?.Percentage, "humidity","%","measurement");
-        await PostEntity("Light", device.EntityName, device.Light?.Lux,"illuminance", "lx","measurement");
-        await PostEntity("Battery", device.EntityName,  device.Battery?.Percentage,"battery","%","measurement");
-        await PostEntity("Location", device.EntityName, $"{device.Location?.Latitude},{device.Location?.Longitude}","enum");
-        await PostEntity("Address", device.EntityName, device.Location?.FormattedAddress,"enum");
+        await PostEntity("Temperature", device.EntityName, device.Temperature?.Celsius,"temperature","sensor", "C","measurement");
+        await PostEntity("SignalStrength", device.EntityName, device.Cellular?.SignalStrength,"enum","sensor");
+        await PostEntity("DBM", device.EntityName, device.Cellular?.Dbm,"signal_strength","sensor","dBm","measurement");
+        await PostEntity("Humidity", device.EntityName, device.Humidity?.Percentage, "humidity","%","sensor","measurement");
+        await PostEntity("Light", device.EntityName, device.Light?.Lux,"illuminance", "lx","sensor","measurement");
+        await PostEntity("Battery", device.EntityName,  device.Battery?.Percentage,"battery","%","sensor","measurement");
+        await PostEntity("Location", device.EntityName, $"{device.Location?.Latitude},{device.Location?.Longitude}","enum","device_tracker");
+        await PostEntity("Address", device.EntityName, device.Location?.FormattedAddress,"enum","sensor");
     }
 
-    private async Task PostEntity(string name, string entityId, object value, string deviceClass, string? unitOfMeasurement = null, string? stateClass = null) {
+    private async Task PostEntity(string name, string entityId, object value, string deviceClass,string deviceType, string? unitOfMeasurement = null, string? stateClass = null) {
 
         using StringContent jsonContent = new(
                 JsonSerializer.Serialize(new
@@ -45,6 +45,6 @@ public class HAService : IHAService {
                 "application/json");
 
         _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _config["HAToken"]);
-        var result = await _client.PostAsync($"{_config["HABaseURL"]}/api/states/sensor.solo_{entityId}_{name}", jsonContent);
+        var result = await _client.PostAsync($"{_config["HABaseURL"]}/api/states/{deviceType}.solo_{entityId}_{name}", jsonContent);
     }
 }
